eb7ef32f019c7509eea086b350ee10ac53786787
Saddique Shafi <saddiqueashafi@gmail.com>
2020-09-22 13:46:26 -0400

    Added MapHandler()

--- Url-Shortener/main.go
+++ Url-Shortener/main.go
@@ -8,8 +8,14 @@ import (
 func main() {
 	mux := defaultMux()
 
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := MapHandler(pathsToUrls, mux)
+
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(":8080", mapHandler)
 }
 
 func defaultMux() *http.ServeMux {
@@ -20,4 +26,19 @@ func defaultMux() *http.ServeMux {
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello World!")
+}
+
+// MapHandler will return an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths (keys in the map) to their corresponding URL
+// (values that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	}
 }
