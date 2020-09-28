a7cf6bfb3a287ea0c5b7b5c8e26d237f3e9e06b7
Saddique Shafi <saddiqueashafi@gmail.com>
2020-09-28 19:14:29 -0400

    Added comments to JSONHandler; Added meta-tags to pathURL

--- Url-Shortener/handler/handler.go
+++ Url-Shortener/handler/handler.go
@@ -42,9 +42,38 @@ func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, err
 	return MapHandler(pathsToUrls, fallback), nil
 }
 
+// JSONHandler will parse the provided JSON and then return a http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding URL
+// If the path is not provided in the JSON, then the fallback http.Handler will be called instead.
+//
+// JSON is expected to be in the format:
+//
+// [{"path": "/some-path", "url": "https://www.some-url.com/demo"}]
+//
+// The only errors that can be returned are all related to having invalid JSON data.
+func JSONHandler(jsonBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	pathURLs, err := parseJSON(jsonBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	pathsToUrls := buildMap(pathURLs)
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
 func parseYaml(yamlBytes []byte) ([]pathURL, error) {
 	var pathURLs []pathURL
 	err := yaml.Unmarshal(yamlBytes, &pathURLs)
+
+	if err != nil {
+		return nil, err
+	}
+	return pathURLs, nil
+}
+
+func parseJSON(jsonBytes []byte) ([]pathURL, error) {
+	var pathURLs []pathURL
+	err := json.Unmarshal(jsonBytes, &pathURLs)
 
 	if err != nil {
 		return nil, err
@@ -60,33 +89,7 @@ func buildMap(pathURLs []pathURL) map[string]string {
 	return pathsToUrls
 }
 
-// type pathURL struct {
-// 	Path string `yaml:"path"`
-// 	URL  string `yaml:"url"`
-// }
-
 type pathURL struct {
-	Path string
-	URL  string
-}
-
-// JSONHandler will
-func JSONHandler(jsonBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	pathURLs, err := parseJSON(jsonBytes)
-	if err != nil {
-		return nil, err
-	}
-
-	pathsToUrls := buildMap(pathURLs)
-	return MapHandler(pathsToUrls, fallback), nil
-}
-
-func parseJSON(jsonBytes []byte) ([]pathURL, error) {
-	var pathURLs []pathURL
-	err := json.Unmarshal(jsonBytes, &pathURLs)
-
-	if err != nil {
-		return nil, err
-	}
-	return pathURLs, nil
+	Path string `json:"path" yaml:"path"`
+	URL  string `json:"url" yaml:"url"`
 }
