aa54fe3d46ab1b9f3c636616abdc39efca05060a
Saddique Shafi <saddiqueashafi@gmail.com>
2020-10-01 16:34:41 -0400

    Added parseJSON()

--- CYOA/main.go
+++ CYOA/main.go
@@ -1,14 +1,37 @@
 package main
 
-import "flag"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
+	jsonFile := getCommandLineFlags()
+	s := parseJSON(*jsonFile)
+	fmt.Printf("%+v", s)
 }
 
 func getCommandLineFlags() *string {
 	jsonFlag := flag.String("file", "gopher.json", "the JSON file with the CYOA story")
 	flag.Parse()
 	return jsonFlag
+}
+
+func parseJSON(jsonFile string) story {
+	f, err := os.Open(jsonFile)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	d := json.NewDecoder(f)
+	var s story
+	if err := d.Decode(&s); err != nil {
+		panic(err)
+	}
+	return s
 }
 
 type story map[string]chapter
