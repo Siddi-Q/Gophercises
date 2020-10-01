075b95e7e3bbd80c24e2973c20f52f61692c1518
Saddique Shafi <saddiqueashafi@gmail.com>
2020-10-01 14:52:30 -0400

    Added story, chapter, and option structs

--- CYOA/main.go
+++ CYOA/main.go
@@ -3,3 +3,16 @@ package main
 func main() {
 
 }
+
+type story map[string]chapter
+
+type chapter struct {
+	Title      string   `json:"title"`
+	Paragraphs []string `json:"story"`
+	Options    []option `json:"options"`
+}
+
+type option struct {
+	Text string `json:"text"`
+	Arc  string `json:"arc"`
+}
