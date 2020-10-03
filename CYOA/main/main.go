b6476d31d651cc269d698951f5cf89b29506abae
Saddique Shafi <saddiqueashafi@gmail.com>
2020-10-03 19:48:33 -0400

    Deleted structs/functions; Added openFile()

--- CYOA/main/main.go
+++ CYOA/main/main.go
@@ -1,48 +1,34 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
+
+	"example.com/story"
 )
 
 func main() {
 	jsonFile := getCommandLineFlags()
-	s := parseJSON(*jsonFile)
+	f := openFile(*jsonFile)
+	s, err := story.ParseJSONStory(f)
+	if err != nil {
+		panic(err)
+	}
+	f.Close()
 	fmt.Printf("%+v", s)
 }
 
 func getCommandLineFlags() *string {
-	jsonFlag := flag.String("file", "gopher.json", "the JSON file with the CYOA story")
+	jsonFlag := flag.String("file", "../gopher.json", "the JSON file with the CYOA story")
 	flag.Parse()
 	return jsonFlag
 }
 
-func parseJSON(jsonFile string) story {
-	f, err := os.Open(jsonFile)
+func openFile(fileName string) *os.File {
+	f, err := os.Open(fileName)
 	if err != nil {
 		panic(err)
 	}
-	defer f.Close()
-
-	d := json.NewDecoder(f)
-	var s story
-	if err := d.Decode(&s); err != nil {
-		panic(err)
-	}
-	return s
-}
-
-type story map[string]chapter
-
-type chapter struct {
-	Title      string   `json:"title"`
-	Paragraphs []string `json:"story"`
-	Options    []option `json:"options"`
-}
-
-type option struct {
-	Text string `json:"text"`
-	Arc  string `json:"arc"`
+	return f
 }
