83ba8bc9713b3bbaebabde3e577e8e1c90fdd77a
Saddique Shafi <saddiqueashafi@gmail.com>
2020-10-15 17:47:04 -0400

    Added getCommandLineFlags()

--- CYOA/cyoa-cli/main.go
+++ CYOA/cyoa-cli/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "flag"
 
+func main() {
+	jsonFile := getCommandLineFlags()
+}
+
+func getCommandLineFlags() *string {
+	jsonFlag := flag.String("file", "../gopher.json", "the JSON file with the CYOA story")
+	flag.Parse()
+	return jsonFlag
 }
