bb17067a4d29592620ec88a65237aa92ad580f07
Saddique Shafi <saddiqueashafi@gmail.com>
2020-10-15 18:06:13 -0400

    Added openFile()

--- CYOA/cyoa-cli/main.go
+++ CYOA/cyoa-cli/main.go
@@ -1,13 +1,25 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 func main() {
 	jsonFile := getCommandLineFlags()
+	openFile(*jsonFile)
 }
 
 func getCommandLineFlags() *string {
 	jsonFlag := flag.String("file", "../gopher.json", "the JSON file with the CYOA story")
 	flag.Parse()
 	return jsonFlag
+}
+
+func openFile(fileName string) *os.File {
+	f, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+	return f
 }
