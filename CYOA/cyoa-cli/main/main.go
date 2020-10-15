d8c52b513b436d8a944e63b05ca2e82710ec82e3
Saddique Shafi <saddiqueashafi@gmail.com>
2020-10-15 19:19:51 -0400

    Parsed json story

--- CYOA/cyoa-cli/main/main.go
+++ CYOA/cyoa-cli/main/main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
+
+	"example.com/story"
 )
 
 func main() {
 	jsonFile := getCommandLineFlags()
-	openFile(*jsonFile)
+	f := openFile(*jsonFile)
+	s, err := story.ParseJSONStory(f)
+	if err != nil {
+		panic(err)
+	}
+	f.Close()
+	fmt.Printf("%+v", s)
 }
 
 func getCommandLineFlags() *string {
-	jsonFlag := flag.String("file", "../gopher.json", "the JSON file with the CYOA story")
+	jsonFlag := flag.String("file", "../../gopher.json", "the JSON file with the CYOA story")
 	flag.Parse()
 	return jsonFlag
 }
