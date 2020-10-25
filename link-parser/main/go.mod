50e487a7c7b55cf12c23f1bd494a73f644476dfb
Saddique Shafi <saddiqueashafi@gmail.com>
2020-10-24 21:23:25 -0400

    Initialized main module

---
+++ link-parser/main/go.mod
@@ -0,0 +1,7 @@
+module main
+
+go 1.15
+
+replace example.com/linkparser => ../linkparser
+
+require example.com/linkparser v0.0.0-00010101000000-000000000000
--- link-parser/main/main.go
+++ link-parser/main/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-	example.com/linkparser
+	"fmt"
+	"strings"
+
+	"example.com/linkparser"
 )
 
+var html = `
+<html>
+<body>
+  <h1>Hello!</h1>
+  <a href="/other-page">A link to another page</a>
+</body>
+</html>
+`
+
 func main() {
-	
+	r := strings.NewReader(html)
+	links, err := linkparser.Parse(r)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", links)
 }
