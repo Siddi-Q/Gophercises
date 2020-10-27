39b1b0b14ffa5c483daf0a5fbc342d93cf8f33b9
Saddique Shafi <saddiqueashafi@gmail.com>
2020-10-27 19:42:54 -0400

    Updated TestParse()

--- link-parser/linkparser/linkparser_test.go
+++ link-parser/linkparser/linkparser_test.go
@@ -1,7 +1,30 @@
 package linkparser
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestParse(t *testing.T) {
+	html := `
+<html>
+<body>
+	<h1>Hello!</h1>
+	<a href="/other-page">A link to another page</a>
+</body>
+</html>
+`
 
+	r := strings.NewReader(html)
+	links, err := Parse(r)
+
+	if len(links) != 1 || err != nil {
+		t.Fatal("Error")
+	}
+
+	wantedLink := Link{Href: "/other-page", Text: "A link to another page"}
+
+	if links[0] != wantedLink {
+		t.Fatal("Error")
+	}
 }
