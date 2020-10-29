dd945e470693552876dc25dc38e08caae4495160
Saddique Shafi <saddiqueashafi@gmail.com>
2020-10-29 16:28:02 -0400

    Updated TestParse() to loop through a set of test cases.

--- link-parser/linkparser/linkparser_test.go
+++ link-parser/linkparser/linkparser_test.go
@@ -6,25 +6,34 @@ import (
 )
 
 func TestParse(t *testing.T) {
-	html := `
+	var tests = []struct {
+		html   string
+		length int
+		err    error
+		links  []Link
+	}{
+		{`
 <html>
 <body>
 	<h1>Hello!</h1>
 	<a href="/other-page">A link to another page</a>
 </body>
 </html>
-`
-
-	r := strings.NewReader(html)
-	links, err := Parse(r)
-
-	if len(links) != 1 || err != nil {
-		t.Fatalf(`Got %d, %v. Want %d, %v.`, len(links), err, 1, nil)
+`, 1, nil, []Link{{Href: "/other-page", Text: "A link to another page"}}},
 	}
 
-	wantedLink := Link{Href: "/other-page", Text: "A link to another page"}
+	for _, test := range tests {
+		r := strings.NewReader(test.html)
+		links, err := Parse(r)
 
-	if links[0] != wantedLink {
-		t.Fatalf(`Got %+v. Want %+v.`, links[0], wantedLink)
+		if len(links) != test.length || err != test.err {
+			t.Fatalf(`Got %d, %v. Want %d, %v.`, len(links), err, test.length, test.err)
+		}
+
+		for i, link := range test.links {
+			if links[i] != link {
+				t.Fatalf(`Got %+v. Want %+v.`, links[i], link)
+			}
+		}
 	}
 }
