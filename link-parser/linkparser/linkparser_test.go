1132400db7384a89908ab0420b7f00def9a653af
Saddique Shafi <saddiqueashafi@gmail.com>
2020-10-30 19:41:35 -0400

    Added another test case, which includes two link tags.

--- link-parser/linkparser/linkparser_test.go
+++ link-parser/linkparser/linkparser_test.go
@@ -20,6 +20,26 @@ func TestParse(t *testing.T) {
 </body>
 </html>
 `, 1, nil, []Link{{Href: "/other-page", Text: "A link to another page"}}},
+		{`
+<html>
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+  <h1>Social stuffs</h1>
+  <div>
+    <a href="https://www.twitter.com/joncalhoun">
+      Check me out on twitter
+      <i class="fa fa-twitter" aria-hidden="true"></i>
+    </a>
+    <a href="https://github.com/gophercises">
+      Gophercises is on <strong>Github</strong>!
+    </a>
+  </div>
+</body>
+</html>
+`, 2, nil, []Link{{Href: "https://www.twitter.com/joncalhoun", Text: "Check me out on twitter"},
+			{Href: "https://github.com/gophercises", Text: "Gophercises is on Github!"}}},
 	}
 
 	for _, test := range tests {
