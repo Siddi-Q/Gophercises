2512c22b472a7c098c4db866dbe8641382bbec99
Saddique Shafi <saddiqueashafi@gmail.com>
2020-10-31 16:07:43 -0400

    Added getLinks()

--- sitemap-builder/main/go.mod
+++ sitemap-builder/main/go.mod
@@ -1,3 +1,7 @@
 module main
 
 go 1.15
+
+replace example.com/linkparser => ../../link-parser/linkparser
+
+require example.com/linkparser v0.0.0-00010101000000-000000000000
--- sitemap-builder/main/main.go
+++ sitemap-builder/main/main.go
@@ -3,15 +3,54 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"example.com/linkparser"
 )
 
 func main() {
-	url := getCommandLineFlags()
-	fmt.Println(*url)
+	urlStr := getCommandLineFlags()
+
+	resp, err := http.Get(*urlStr)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	reqURL := resp.Request.URL
+	baseURL := &url.URL{
+		Scheme: reqURL.Scheme,
+		Host:   reqURL.Host,
+	}
+	base := baseURL.String()
+
+	pages := getLinks(resp.Body, base)
+	for _, page := range pages {
+		fmt.Println(page)
+	}
+
 }
 
 func getCommandLineFlags() *string {
 	urlFlag := flag.String("url", "https://gophercises.com", "the url that you want to build a sitemap for")
 	flag.Parse()
 	return urlFlag
+}
+
+func getLinks(r io.Reader, base string) []string {
+	links, _ := linkparser.Parse(r)
+
+	var formattedLinks []string
+	for _, link := range links {
+		switch {
+		case strings.HasPrefix(link.Href, "/"):
+			formattedLinks = append(formattedLinks, base+link.Href)
+		case strings.HasPrefix(link.Href, "http"):
+			formattedLinks = append(formattedLinks, link.Href)
+		}
+	}
+	return formattedLinks
 }
