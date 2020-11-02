32444665fba6fa0a259eb6b97c98c279b8ed3f6a
Saddique Shafi <saddiqueashafi@gmail.com>
2020-11-02 17:07:24 -0500

    Added bfs()

--- sitemap-builder/main/main.go
+++ sitemap-builder/main/main.go
@@ -13,8 +13,8 @@ import (
 
 func main() {
 	urlStr := getCommandLineFlags()
+	links := bfs(*urlStr)
 
-	links := getAllLinks(*urlStr)
 	for _, link := range links {
 		fmt.Println(link)
 	}
@@ -24,6 +24,32 @@ func getCommandLineFlags() *string {
 	urlFlag := flag.String("url", "https://gophercises.com", "the url that you want to build a sitemap for")
 	flag.Parse()
 	return urlFlag
+}
+
+func bfs(urlStr string) []string {
+	visited := make(map[string]struct{})
+	queue := map[string]struct{}{
+		urlStr: struct{}{},
+	}
+
+	for 0 < len(queue) {
+		nextQueue := make(map[string]struct{})
+		for key := range queue {
+			if _, ok := visited[key]; !ok {
+				visited[key] = struct{}{}
+				for _, link := range getAllLinks(key) {
+					nextQueue[link] = struct{}{}
+				}
+			}
+		}
+		queue = nextQueue
+	}
+
+	links := make([]string, 0, len(visited))
+	for link := range visited {
+		links = append(links, link)
+	}
+	return links
 }
 
 func getAllLinks(urlStr string) []string {
