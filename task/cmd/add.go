1bb61ea86e1dcaed03caa3a8b2794175cf3f5050
Saddique Shafi <saddiqueashafi@gmail.com>
2020-11-09 14:05:01 -0500

    Created add.go; Created the add command

---
+++ task/cmd/add.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(addCmd)
+}
+
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Adds a task to your task list.",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("add called")
+	},
+}
