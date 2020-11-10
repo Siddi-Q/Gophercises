843f007e7e348a4c1725c1119c2001de5bf8748e
Saddique Shafi <saddiqueashafi@gmail.com>
2020-11-10 16:28:23 -0500

    Updated doCmd to get list of ids from cmd args

--- task/cmd/do.go
+++ task/cmd/do.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -15,5 +16,16 @@ var doCmd = &cobra.Command{
 	Short: "Marks a task as complete",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("do called")
+		var ids []int
+
+		for _, arg := range args {
+			id, err := strconv.Atoi(arg)
+			if err != nil {
+				fmt.Println("Failed to parse the argument:", arg)
+			} else {
+				ids = append(ids, id)
+			}
+		}
+		fmt.Println(ids)
 	},
 }
