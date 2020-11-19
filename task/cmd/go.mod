6cbe70d7cbd2f0352fda26e5d86c4f5433fbfb8f
Saddique Shafi <saddiqueashafi@gmail.com>
2020-11-19 13:13:10 -0500

    Updated listCmd; connected it to boltdb

--- task/cmd/go.mod
+++ task/cmd/go.mod
@@ -2,4 +2,9 @@ module example.com/cmd
 
 go 1.15
 
-require github.com/spf13/cobra v1.1.1
+replace example.com/db => ../db
+
+require (
+	example.com/db v0.0.0-00010101000000-000000000000
+	github.com/spf13/cobra v1.1.1
+)
--- task/cmd/list.go
+++ task/cmd/list.go
@@ -2,8 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
+
+	"example.com/db"
 )
 
 func init() {
@@ -14,6 +17,20 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Lists all of your task",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		tasks, err := db.ReadAllTasks()
+
+		if err != nil {
+			fmt.Println("Something went wrong:", err.Error())
+			os.Exit(1)
+		}
+
+		if len(tasks) == 0 {
+			fmt.Println("You have no tasks to complete!")
+		} else {
+			fmt.Println("You have the following tasks:")
+			for i, task := range tasks {
+				fmt.Printf("%d. %s\n", i+1, task.Value)
+			}
+		}
 	},
 }
