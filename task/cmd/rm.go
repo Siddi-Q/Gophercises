b8b859ef0143b137aba2272092c595e3306c8d25
Saddique Shafi <saddiqueashafi@gmail.com>
2020-11-23 17:40:23 -0500

    Created the rm command

--- task/cmd/rm.go
+++ task/cmd/rm.go
@@ -1,1 +1,19 @@
 package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(rmCmd)
+}
+
+var rmCmd = &cobra.Command{
+	Use:   "rm",
+	Short: "Deletes a task",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Rm")
+	},
+}
