884c3c5df9d7dcad76f43d3ab9da33cbfbd6e4dd
Saddique Shafi <saddiqueashafi@gmail.com>
2020-11-24 18:45:28 -0500

    Updated Task struct, CreateTask() and  ReadAllTasks()

--- task/db/db.go
+++ task/db/db.go
@@ -1,7 +1,99 @@
+// package db
+
+// import (
+// 	"encoding/binary"
+// 	"time"
+
+// 	"github.com/boltdb/bolt"
+// )
+
+// var taskBucket = []byte("tasks")
+// var db *bolt.DB
+
+// // Task is
+// type Task struct {
+// 	Key   int
+// 	Value string
+// }
+
+// // InitDB will
+// func InitDB(dbPath string) error {
+// 	var err error
+// 	db, err = bolt.Open(dbPath, 0600, &bolt.Options{Timeout: 1 * time.Second})
+
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	return db.Update(func(tx *bolt.Tx) error {
+// 		_, err := tx.CreateBucketIfNotExists(taskBucket)
+// 		return err
+// 	})
+// }
+
+// // CreateTask will
+// func CreateTask(task string) (int, error) {
+// 	var id int
+// 	err := db.Update(func(tx *bolt.Tx) error {
+// 		b := tx.Bucket(taskBucket)
+// 		id64, _ := b.NextSequence()
+// 		id = int(id64)
+// 		key := itob(id)
+// 		return b.Put(key, []byte(task))
+// 	})
+
+// 	if err != nil {
+// 		return -1, err
+// 	}
+
+// 	return id, nil
+// }
+
+// // ReadAllTasks will
+// func ReadAllTasks() ([]Task, error) {
+// 	var tasks []Task
+// 	err := db.View(func(tx *bolt.Tx) error {
+// 		b := tx.Bucket(taskBucket)
+// 		c := b.Cursor()
+
+// 		for k, v := c.First(); k != nil; k, v = c.Next() {
+// 			tasks = append(tasks, Task{
+// 				Key:   btoi(k),
+// 				Value: string(v),
+// 			})
+// 		}
+// 		return nil
+// 	})
+
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return tasks, nil
+// }
+
+// // DeleteTask will
+// func DeleteTask(key int) error {
+// 	return db.Update(func(tx *bolt.Tx) error {
+// 		b := tx.Bucket(taskBucket)
+// 		return b.Delete(itob(key))
+// 	})
+// }
+
+// func itob(v int) []byte {
+// 	b := make([]byte, 8)
+// 	binary.BigEndian.PutUint64(b, uint64(v))
+// 	return b
+// }
+
+// func btoi(b []byte) int {
+// 	return int(binary.BigEndian.Uint64(b))
+// }
+
 package db
 
 import (
 	"encoding/binary"
+	"encoding/json"
 	"time"
 
 	"github.com/boltdb/bolt"
@@ -12,8 +104,9 @@ var db *bolt.DB
 
 // Task is
 type Task struct {
-	Key   int
-	Value string
+	ID          int
+	Description string
+	Completed   bool
 }
 
 // InitDB will
@@ -39,7 +132,20 @@ func CreateTask(task string) (int, error) {
 		id64, _ := b.NextSequence()
 		id = int(id64)
 		key := itob(id)
-		return b.Put(key, []byte(task))
+
+		task := &Task{
+			ID:          id,
+			Description: task,
+			Completed:   false,
+		}
+
+		taskEnc, err := json.Marshal(task)
+
+		if err != nil {
+			return err
+		}
+
+		return b.Put(key, taskEnc)
 	})
 
 	if err != nil {
@@ -57,10 +163,14 @@ func ReadAllTasks() ([]Task, error) {
 		c := b.Cursor()
 
 		for k, v := c.First(); k != nil; k, v = c.Next() {
-			tasks = append(tasks, Task{
-				Key:   btoi(k),
-				Value: string(v),
-			})
+			var task *Task
+			err := json.Unmarshal(v, &task)
+
+			if err != nil {
+				return err
+			}
+
+			tasks = append(tasks, *task)
 		}
 		return nil
 	})
