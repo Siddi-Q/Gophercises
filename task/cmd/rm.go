9b006a230ee0930a7c5e28fab0b855096652bf4e
Saddique Shafi <saddiqueashafi@gmail.com>
2020-11-26 16:46:22 -0500

    Updated rmCmd; connected it to boltdb

--- task/cmd/rm.go
+++ task/cmd/rm.go
@@ -2,7 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
+	"example.com/db"
 	"github.com/spf13/cobra"
 )
 
@@ -14,6 +17,38 @@ var rmCmd = &cobra.Command{
 	Use:   "rm",
 	Short: "Deletes a task",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Rm")
+		var ids []int
+
+		for _, arg := range args {
+			id, err := strconv.Atoi(arg)
+			if err != nil {
+				fmt.Println("Failed to parse the argument:", arg)
+			} else {
+				ids = append(ids, id)
+			}
+		}
+
+		tasks, err := db.ReadAllTasks()
+
+		if err != nil {
+			fmt.Println("Something went wrong:", err.Error())
+			os.Exit(1)
+		} else {
+			for _, id := range ids {
+				if id <= 0 || id > len(tasks) {
+					fmt.Println("Invalid task number:", id)
+					continue
+				}
+
+				task := tasks[id-1]
+				err := db.DeleteTask(task.ID)
+
+				if err != nil {
+					fmt.Printf("Failed to mark \"%d\" as completed. Error: %s\n", id, err.Error())
+				} else {
+					fmt.Printf("Marked \"%d. %s\" as completed.\n", id, task.Description)
+				}
+			}
+		}
 	},
 }
