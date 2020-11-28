d1e4a47fc6198e7073bb967f93f08a1bd5b6a535
Saddique Shafi <saddiqueashafi@gmail.com>
2020-11-28 14:16:27 -0500

    Added completed.go, completedCmd, and connected it to boltdb

---
+++ task/cmd/completed.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"example.com/db"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(completedCmd)
+}
+
+var completedCmd = &cobra.Command{
+	Use:   "completed",
+	Short: "Lists all of your completed tasks",
+	Run: func(cmd *cobra.Command, args []string) {
+		tasks, err := db.ReadSomeTasks(true)
+
+		if err != nil {
+			fmt.Println("Something went wrong:", err.Error())
+			os.Exit(1)
+		}
+
+		if len(tasks) == 0 {
+			fmt.Println("You have not completed any tasks!")
+		} else {
+			fmt.Println("You have completed the following tasks:")
+			for i, task := range tasks {
+				fmt.Printf("%d. %s\n", i+1, task.Description)
+			}
+		}
+	},
+}
