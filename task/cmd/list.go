9713c3019d39a51f797bc0254f14b8dcc0c4b697
Saddique Shafi <saddiqueashafi@gmail.com>
2020-11-28 17:08:28 -0500

    Updated listCmd to have a type flag; Added printAllTasks() and printSomeTasks()

--- task/cmd/list.go
+++ task/cmd/list.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 
@@ -11,33 +12,79 @@ import (
 
 func init() {
 	RootCmd.AddCommand(listCmd)
+	listCmd.Flags().StringVarP(&taskType, "type", "t", "all", "Lists certain type of tasks.\n all: all tasks\n c: completed tasks\n nc: not completed tasks\n")
 }
+
+var taskType string
 
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Lists all of your tasks",
 	Run: func(cmd *cobra.Command, args []string) {
-		tasks, err := db.ReadAllTasks()
-
-		if err != nil {
-			fmt.Println("Something went wrong:", err.Error())
-			os.Exit(1)
+		taskType = strings.ToLower(taskType)
+		switch taskType {
+		case "all":
+			printAllTasks()
+		case "c":
+			printSomeTasks(true)
+		case "nc":
+			printSomeTasks(false)
+		default:
+			fmt.Println("Unrecognized type. Please try again!")
 		}
+	},
+}
 
+func printAllTasks() {
+	tasks, err := db.ReadAllTasks()
+
+	if err != nil {
+		fmt.Println("Something went wrong:", err.Error())
+		os.Exit(1)
+	}
+
+	if len(tasks) == 0 {
+		fmt.Println("You have no tasks to complete!")
+	} else {
+		fmt.Println("You have the following tasks:")
+		for i, task := range tasks {
+			var status string
+			if task.Completed {
+				status = "Completed"
+			} else {
+				status = "Not Completed"
+			}
+
+			fmt.Printf("%d. %s | %s\n", i+1, task.Description, status)
+		}
+	}
+}
+
+func printSomeTasks(completed bool) {
+	tasks, err := db.ReadSomeTasks(completed)
+
+	if err != nil {
+		fmt.Println("Something went wrong:", err.Error())
+		os.Exit(1)
+	}
+
+	if completed {
+		if len(tasks) == 0 {
+			fmt.Println("You have not completed any tasks!")
+		} else {
+			fmt.Println("You have completed the following tasks:")
+			for i, task := range tasks {
+				fmt.Printf("%d. %s\n", i+1, task.Description)
+			}
+		}
+	} else {
 		if len(tasks) == 0 {
 			fmt.Println("You have no tasks to complete!")
 		} else {
-			fmt.Println("You have the following tasks:")
+			fmt.Println("You have not completed the following tasks:")
 			for i, task := range tasks {
-				var status string
-				if task.Completed {
-					status = "Completed"
-				} else {
-					status = "Not Completed"
-				}
-
-				fmt.Printf("%d. %s | %s\n", i+1, task.Description, status)
+				fmt.Printf("%d. %s\n", i+1, task.Description)
 			}
 		}
-	},
+	}
 }
