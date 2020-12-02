21b0855ce2e73474f54fb59a6544a26839025f1a
Saddique Shafi <saddiqueashafi@gmail.com>
2020-12-02 14:41:39 -0500

    Updated completedCmd to use duration flag

--- task/cmd/completed.go
+++ task/cmd/completed.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"example.com/db"
@@ -20,6 +21,7 @@ var completedCmd = &cobra.Command{
 	Use:   "completed",
 	Short: "Lists all of your completed tasks",
 	Run: func(cmd *cobra.Command, args []string) {
+		now := time.Now()
 		tasks, err := db.ReadSomeTasks(true)
 
 		if err != nil {
@@ -27,14 +29,72 @@ var completedCmd = &cobra.Command{
 			os.Exit(1)
 		}
 
-		if len(tasks) == 0 {
-			fmt.Println("You have not completed any tasks!")
-		} else {
-			fmt.Println("You have completed the following tasks:")
-			for i, task := range tasks {
-				fmt.Printf("%d. %s | Completed on %v\n", i+1, task.Description, task.CompletedDate.Format("Monday, January 2, 2006 03:04:05 PM"))
+		duration = strings.ToLower(duration)
+		switch duration {
+		case "forever":
+			fmt.Println("forever")
+			if len(tasks) == 0 {
+				fmt.Println("You have not completed any tasks!")
+			} else {
+				fmt.Println("You have completed the following tasks:")
+				for i, task := range tasks {
+					fmt.Printf("%d. %s | Completed on %v\n", i+1, task.Description, task.CompletedDate.Format("Monday, January 2, 2006 03:04:05 PM"))
+				}
 			}
+		case "today":
+			fmt.Println("today")
+			var filteredTasks []db.Task
+			for _, task := range tasks {
+				if isSameDay(task.CompletedDate, now) {
+					filteredTasks = append(filteredTasks, task)
+				}
+			}
+		case "24h":
+			fmt.Println("24h")
+			var filteredTasks []db.Task
+			for _, task := range tasks {
+				if isWithin(task.CompletedDate, now, time.Hour*24) {
+					filteredTasks = append(filteredTasks, task)
+				}
+			}
+
+			if len(filteredTasks) == 0 {
+				fmt.Println("You have not completed any tasks!")
+			} else {
+				fmt.Println("You have completed the following tasks:")
+				for i, task := range filteredTasks {
+					fmt.Printf("%d. %s | Completed on %v\n", i+1, task.Description, task.CompletedDate.Format("Monday, January 2, 2006 03:04:05 PM"))
+				}
+			}
+		case "12h":
+			fmt.Println("12h")
+			var filteredTasks []db.Task
+			for _, task := range tasks {
+				if isWithin(task.CompletedDate, now, time.Hour*12) {
+					filteredTasks = append(filteredTasks, task)
+				}
+			}
+
+			if len(filteredTasks) == 0 {
+				fmt.Println("You have not completed any tasks!")
+			} else {
+				fmt.Println("You have completed the following tasks:")
+				for i, task := range filteredTasks {
+					fmt.Printf("%d. %s | Completed on %v\n", i+1, task.Description, task.CompletedDate.Format("Monday, January 2, 2006 03:04:05 PM"))
+				}
+			}
+		default:
+			fmt.Println("Unrecognized duration. Please try again!")
 		}
+
+		// if len(tasks) == 0 {
+		// 	fmt.Println("You have not completed any tasks!")
+		// } else {
+		// 	fmt.Println("You have completed the following tasks:")
+		// 	for i, task := range tasks {
+		// 		fmt.Printf("%d. %s | Completed on %v\n", i+1, task.Description, task.CompletedDate.Format("Monday, January 2, 2006 03:04:05 PM"))
+		// 	}
+		// }
 	},
 }
 
