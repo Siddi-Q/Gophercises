d336b4274c8b55df2424dcc9d656be11b7c85df6
Saddique Shafi <saddiqueashafi@gmail.com>
2020-12-09 13:45:02 -0500

    Added all flag to rmCmd; Updated rmCmd to use all flag

--- task/cmd/rm.go
+++ task/cmd/rm.go
@@ -12,42 +12,49 @@ import (
 
 func init() {
 	RootCmd.AddCommand(rmCmd)
+	rmCmd.Flags().BoolVarP(&rmAll, "all", "a", false, "Removes all tasks\n")
 }
+
+var rmAll bool
 
 var rmCmd = &cobra.Command{
 	Use:   "rm",
 	Short: "Deletes a task",
 	Run: func(cmd *cobra.Command, args []string) {
-		var ids []int
-
-		for _, arg := range args {
-			id, err := strconv.Atoi(arg)
-			if err != nil {
-				color.New(color.FgRed).Println("Failed to parse the argument:", arg)
-			} else {
-				ids = append(ids, id)
-			}
-		}
-
-		tasks, err := db.ReadAllTasks()
-
-		if err != nil {
-			fmt.Println("Something went wrong:", err.Error())
-			os.Exit(1)
+		if rmAll {
+			db.DeleteBucket()
 		} else {
-			for _, id := range ids {
-				if id <= 0 || id > len(tasks) {
-					color.New(color.FgRed).Println("Invalid task number:", id)
-					continue
-				}
+			var ids []int
 
-				task := tasks[id-1]
-				err := db.DeleteTask(task.ID)
-
+			for _, arg := range args {
+				id, err := strconv.Atoi(arg)
 				if err != nil {
-					fmt.Printf("Failed to delete \"%d\". Error: %s\n", id, err.Error())
+					color.New(color.FgRed).Println("Failed to parse the argument:", arg)
 				} else {
-					color.New(color.FgYellow).Printf("Deleted \"%d. %s\".\n", id, task.Description)
+					ids = append(ids, id)
+				}
+			}
+
+			tasks, err := db.ReadAllTasks()
+
+			if err != nil {
+				fmt.Println("Something went wrong:", err.Error())
+				os.Exit(1)
+			} else {
+				for _, id := range ids {
+					if id <= 0 || id > len(tasks) {
+						color.New(color.FgRed).Println("Invalid task number:", id)
+						continue
+					}
+
+					task := tasks[id-1]
+					err := db.DeleteTask(task.ID)
+
+					if err != nil {
+						fmt.Printf("Failed to delete \"%d\". Error: %s\n", id, err.Error())
+					} else {
+						color.New(color.FgYellow).Printf("Deleted \"%d. %s\".\n", id, task.Description)
+					}
 				}
 			}
 		}
