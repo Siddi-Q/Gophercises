d83cdb844938ebb987d6207efbb1ad1559a6a698
Saddique Shafi <saddiqueashafi@gmail.com>
2020-12-09 13:42:49 -0500

    Added DeleteBucket()

--- task/db/db.go
+++ task/db/db.go
@@ -160,6 +160,13 @@ func DeleteTask(key int) error {
 	})
 }
 
+// DeleteBucket will
+func DeleteBucket() error {
+	return db.Update(func(tx *bolt.Tx) error {
+		return tx.DeleteBucket(taskBucket)
+	})
+}
+
 func itob(v int) []byte {
 	b := make([]byte, 8)
 	binary.BigEndian.PutUint64(b, uint64(v))
