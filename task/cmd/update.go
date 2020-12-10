0e01a4d142dc4d1c983c170fb397566841a2a951
Saddique Shafi <saddiqueashafi@gmail.com>
2020-12-10 15:32:04 -0500

    Added update.go; Added updateCmd

---
+++ task/cmd/update.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"example.com/db"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(updateCmd)
+}
+
+var updateCmd = &cobra.Command{
+	Use:   "update",
+	Short: "Updates a task's description",
+	Run: func(cmd *cobra.Command, args []string) {
+		id, err := strconv.Atoi(args[0])
+		if err != nil {
+			fmt.Println("Failed to parse the argument:", args[0])
+		}
+
+		description := strings.Join(args[1:], " ")
+
+		tasks, err := db.ReadAllTasks()
+
+		if err != nil {
+			fmt.Println("Something went wrong:", err.Error())
+			os.Exit(1)
+		} else {
+			if id <= 0 || id > len(tasks) {
+				fmt.Println("Invalid task number:", id)
+			} else {
+				task := tasks[id-1]
+				err := db.UpdateTaskDescription(task.ID, description)
+
+				if err != nil {
+					fmt.Printf("Failed to update \"%d. %s\". Error: %s\n", id, task.Description, err.Error())
+				} else {
+					fmt.Printf("Updated task's description from \"%d. %s\" to \"%d. %s\".\n", id, task.Description, id, description)
+				}
+			}
+		}
+	},
+}
