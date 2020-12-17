8422b9ce92c5fb0e721c520fdcc9e7697e9ce131
Saddique Shafi <saddiqueashafi@gmail.com>
2020-12-17 13:54:25 -0500

    Added createDB() and resetDB()

--- pnn/main.go
+++ pnn/main.go
@@ -1,9 +1,57 @@
 package main
 
-import "bytes"
+import (
+	"bytes"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "password"
+	dbname   = "phonedb"
+)
 
 func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
+	db, err := sql.Open("postgres", psqlInfo)
+	must(err)
+	err = resetDB(db, dbname)
+	must(err)
+	db.Close()
 
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
+	db, err = sql.Open("postgres", psqlInfo)
+	must(err)
+	defer db.Close()
+
+	must(db.Ping())
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func resetDB(db *sql.DB, dbName string) error {
+	_, err := db.Exec("DROP DATABASE IF EXISTS " + dbName)
+	if err != nil {
+		return err
+	}
+	return createDB(db, dbName)
+}
+
+func createDB(db *sql.DB, dbName string) error {
+	_, err := db.Exec("CREATE DATABASE " + dbName)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func normalizePhoneNumber(pn string) string {
