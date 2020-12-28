06f6b9acc678096993761431aa4f66896c5ad3eb
Saddique Shafi <saddiqueashafi@gmail.com>
2020-12-28 16:50:38 -0500

    Added documentation to itob() and btoi()

--- task/db/db.go
+++ task/db/db.go
@@ -198,12 +198,14 @@ func DeleteBucket() error {
 	})
 }
 
+// itob converts an int to a byte slice
 func itob(v int) []byte {
 	b := make([]byte, 8)
 	binary.BigEndian.PutUint64(b, uint64(v))
 	return b
 }
 
+// btoi converts a byte slice to an int
 func btoi(b []byte) int {
 	return int(binary.BigEndian.Uint64(b))
 }
